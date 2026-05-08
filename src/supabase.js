5979fd7adcfabf0b935678cb82b3965c911c73f6
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-08 10:15:01 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://hcoklqocsuyfquhvvohz.supabase.co'
 
-const supabaseKey = 'sb_publishable_iwwfdDeBmJZd7lsT01cs_A_MnIeX2aZ'
+const supabaseKey =
+  'sb_publishable_iwwfdDeBmJZd7lsT01cs_A_MnIeX2aZ'
 
 export const supabase = createClient(
   supabaseUrl,
