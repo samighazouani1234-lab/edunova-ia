83c03cdf940a090bb5ae8e3ea5d4a3eec21278a3
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:35:17 +0200

    Update primaryEnglish.js

--- src/data/primaryEnglish.js
+++ src/data/primaryEnglish.js
@@ -1,1 +1,24 @@
+const primaryEnglish = [
+  {
+    title: "Vocabulaire anglais",
+    lesson: "Apprendre les mots simples du quotidien en anglais.",
+    video: "https://www.youtube-nocookie.com/embed/hq3yfQnllfQ",
+    objectives: [
+      "Comprendre les mots simples",
+      "Prononcer correctement",
+      "Utiliser le vocabulaire",
+    ],
+    exercises: [
+      {
+        question: "Comment dit-on 'chat' en anglais ?",
+        answer: "cat",
+      },
+      {
+        question: "Comment dit-on 'chien' en anglais ?",
+        answer: "dog",
+      },
+    ],
+  },
+];
 
+export default primaryEnglish;
