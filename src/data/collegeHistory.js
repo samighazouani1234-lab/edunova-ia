8560dc0d1f1adced58334ec65a56c08e58a31ddd
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:38:20 +0200

    Update collegeHistory.js

--- src/data/collegeHistory.js
+++ src/data/collegeHistory.js
@@ -1,1 +1,19 @@
+const collegeHistory = [
+  {
+    title: "La Révolution française",
+    lesson: "Comprendre les événements de 1789.",
+    video: "https://www.youtube-nocookie.com/embed/zY26WwzybM8",
+    objectives: [
+      "Comprendre la révolution",
+      "Connaître les dates importantes",
+    ],
+    exercises: [
+      {
+        question: "En quelle année commence la Révolution française ?",
+        answer: "1789",
+      },
+    ],
+  },
+];
 
+export default collegeHistory;
