d2ae505208b49d859b775450e73956bdaaafb5b8
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:37:57 +0200

    Update primaryHistory.js

--- src/data/primaryHistory.js
+++ src/data/primaryHistory.js
@@ -1,1 +1,19 @@
+const primaryHistory = [
+  {
+    title: "Les continents",
+    lesson: "Découvrir les continents du monde.",
+    video: "https://www.youtube-nocookie.com/embed/eTFE7xqSx8U",
+    objectives: [
+      "Nommer les continents",
+      "Situer les continents",
+    ],
+    exercises: [
+      {
+        question: "Sur quel continent se trouve la France ?",
+        answer: "europe",
+      },
+    ],
+  },
+];
 
+export default primaryHistory;
