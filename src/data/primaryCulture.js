eb5bbbab22de6c551ce4fa4d870e900b7712ad0e
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:39:16 +0200

    Create primaryCulture.js

---
+++ src/data/primaryCulture.js
@@ -0,0 +1,19 @@
+const primaryCulture = [
+  {
+    title: "Culture générale",
+    lesson: "Découvrir le monde et apprendre des faits intéressants.",
+    video: "https://www.youtube-nocookie.com/embed/WXuK6gekU1Y",
+    objectives: [
+      "Développer la curiosité",
+      "Découvrir le monde",
+    ],
+    exercises: [
+      {
+        question: "Combien y a-t-il de jours dans une semaine ?",
+        answer: "7",
+      },
+    ],
+  },
+];
+
+export default primaryCulture;
