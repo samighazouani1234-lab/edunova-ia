0d55bc917483e0720e3aa56d08f8c7006682aae6
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:35:54 +0200

    Update collegeEnglish.js

--- src/data/collegeEnglish.js
+++ src/data/collegeEnglish.js
@@ -1,1 +1,23 @@
+const collegeEnglish = [
+  {
+    title: "Present simple",
+    lesson: "Utiliser le présent simple en anglais.",
+    video: "https://www.youtube-nocookie.com/embed/hq3yfQnllfQ",
+    objectives: [
+      "Construire une phrase",
+      "Conjuguer au présent",
+    ],
+    exercises: [
+      {
+        question: "Complète : She ___ to school.",
+        answer: "goes",
+      },
+      {
+        question: "Complète : I ___ football.",
+        answer: "play",
+      },
+    ],
+  },
+];
 
+export default collegeEnglish;
