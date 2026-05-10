04d566552ba131fd7b56d51fbf01010f28a30565
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 11:17:19 +0200

    Update collegeMath.js

--- src/data/collegeMath.js
+++ src/data/collegeMath.js
@@ -1,1 +1,97 @@
+const collegeMath = [
+  {
+    title: "Calcul numérique",
+    video: "https://www.youtube.com/embed/CnxrBYxzur8",
+    lesson:
+      "Priorités opératoires, fractions, nombres relatifs et puissances.",
+    objectives: [
+      "Priorités",
+      "Relatifs",
+      "Puissances",
+      "Expressions",
+    ],
+    exercises: [
+      {
+        question: "Calcule : 3 + 2 × 5",
+        answer: "13",
+        choices: ["13", "25", "10", "15"],
+      },
+      {
+        question: "Calcule : -3 + 8",
+        answer: "5",
+        choices: ["5", "11", "-5", "2"],
+      },
+      {
+        question: "Calcule : 2²",
+        answer: "4",
+        choices: ["4", "2", "8", "6"],
+      },
+      {
+        question: "Calcule : 4 × (2 + 3)",
+        answer: "20",
+        choices: ["20", "14", "10", "24"],
+      },
+    ],
+  },
+  {
+    title: "Équations",
+    video: "https://www.youtube.com/embed/N4eWBCGQOLI",
+    lesson:
+      "Résoudre une équation, isoler l’inconnue et vérifier une solution.",
+    objectives: [
+      "Inconnue",
+      "Opérations inverses",
+      "Vérifier",
+      "Problèmes",
+    ],
+    exercises: [
+      {
+        question: "Résous : x + 8 = 15",
+        answer: "x = 7",
+        choices: ["x = 7", "x = 5", "x = 8", "x = 10"],
+      },
+      {
+        question: "Résous : x - 4 = 10",
+        answer: "x = 14",
+        choices: ["x = 14", "x = 6", "x = 10", "x = 4"],
+      },
+      {
+        question: "Résous : 2x = 10",
+        answer: "x = 5",
+        choices: ["x = 5", "x = 10", "x = 2", "x = 8"],
+      },
+    ],
+  },
+  {
+    title: "Proportionnalité",
+    video: "https://www.youtube.com/embed/xZGxe8eE8-c",
+    lesson:
+      "Tableaux, pourcentages, échelles et problèmes de vitesse.",
+    objectives: [
+      "Tableaux",
+      "Pourcentages",
+      "Échelles",
+      "Vitesse",
+    ],
+    exercises: [
+      {
+        question: "50% de 80 vaut combien ?",
+        answer: "40",
+        choices: ["40", "20", "80", "60"],
+      },
+      {
+        question: "10% de 200 vaut combien ?",
+        answer: "20",
+        choices: ["20", "10", "40", "30"],
+      },
+      {
+        question:
+          "Si 2 cahiers coûtent 6€, combien coûtent 4 cahiers ?",
+        answer: "12",
+        choices: ["12", "10", "14", "8"],
+      },
+    ],
+  },
+];
 
+export default collegeMath;
