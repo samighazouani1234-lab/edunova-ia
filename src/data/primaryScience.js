07bfae880a228a2be8faed87f1b6f4c1c5258d6a
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:37:11 +0200

    Update primaryScience.js

--- src/data/primaryScience.js
+++ src/data/primaryScience.js
@@ -1,1 +1,23 @@
+const primaryScience = [
+  {
+    title: "Les animaux",
+    lesson: "Découvrir les animaux et leurs familles.",
+    video: "https://www.youtube-nocookie.com/embed/q0ad1e0JSxE",
+    objectives: [
+      "Reconnaître les animaux",
+      "Classer les animaux",
+    ],
+    exercises: [
+      {
+        question: "Le lion est-il un mammifère ?",
+        answer: "oui",
+      },
+      {
+        question: "Quel animal pond des œufs ?",
+        answer: "la poule",
+      },
+    ],
+  },
+];
 
+export default primaryScience;
