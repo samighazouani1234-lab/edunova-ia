a77e37ff2f60b2ee4a9040eabccecc6d0480bb37
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:37:35 +0200

    Update collegeScience.js

--- src/data/collegeScience.js
+++ src/data/collegeScience.js
@@ -1,1 +1,23 @@
+const collegeScience = [
+  {
+    title: "Le système solaire",
+    lesson: "Comprendre les planètes et le soleil.",
+    video: "https://www.youtube-nocookie.com/embed/libKVRa01L8",
+    objectives: [
+      "Identifier les planètes",
+      "Comprendre le système solaire",
+    ],
+    exercises: [
+      {
+        question: "Quelle planète est la plus proche du soleil ?",
+        answer: "mercure",
+      },
+      {
+        question: "La Terre tourne autour de quoi ?",
+        answer: "du soleil",
+      },
+    ],
+  },
+];
 
+export default collegeScience;
