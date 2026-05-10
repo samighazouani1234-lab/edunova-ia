17595b4688ab4d2b93a7db7f686a1e621be2e51e
samighazouani1234-lab <sami.ghazouani1234@gmail.com>
2026-05-10 12:39:58 +0200

    Create collegeCulture.js

---
+++ src/data/collegeCulture.js
@@ -0,0 +1,19 @@
+const collegeCulture = [
+  {
+    title: "Culture générale avancée",
+    lesson: "Approfondir ses connaissances générales.",
+    video: "https://www.youtube-nocookie.com/embed/WXuK6gekU1Y",
+    objectives: [
+      "Renforcer la culture générale",
+      "Analyser des informations",
+    ],
+    exercises: [
+      {
+        question: "Quelle est la capitale du Japon ?",
+        answer: "tokyo",
+      },
+    ],
+  },
+];
+
+export default collegeCulture;
